9f1a2d097e3133b55b5371b74045a2a6bedc88a1
Dar7oo <143949654+dario-falc@users.noreply.github.com>
2025-10-23 20:19:09 +0200

    Updated data extraction

--- main.py
+++ main.py
@@ -15,7 +15,23 @@ def get_applet_triggers(applet):
     return trigger_filter_codes
 
 
-def get_applet_action(applet):
+def get_applet_actions(applet):
+    action_filter_codes = []
+    
+    action_details = applet["action_details"]
+    for elem in action_details:
+        if "Filter code method" in elem["details"].keys():
+            action_filter_codes.append(elem["details"]["Filter code method"])
+
+    if action_filter_codes:
+        action_filter_codes = sorted(list(set(action_filter_codes)))
+    else:
+        action_filter_codes = "No actions available for this applet"
+    
+    return action_filter_codes
+
+
+def get_applet_skip(applet):
     return applet["action_developer_info"]["Filter code method"]
 
 
@@ -44,35 +60,19 @@ def match(model, intent, triggers):
 
 
 if __name__ == "__main__":
-    # with open("data/generated_filtercode_from_intent.jsonl", "r", encoding="utf-8") as f:
-    #     data = f.readlines()
-
-    with open("data/Json200.jsonl", "r", encoding="utf-8") as f:
+    with open("data/generated_filtercode_from_intent.jsonl", "r", encoding="utf-8") as f:
         data = f.readlines()
 
-
-    # For testing
-    start = 0
-    n_to_insert = 20
-    
     applets = []
-    n_inserted = 0
-    i=0
+    for row in data:#[:5]:
+        entry = json.loads(row)
+        applets.append(entry)
 
-    while n_inserted!=n_to_insert:
-        entry = json.loads(data[start+i])
-    
-        # Keep only "tipologia = Corretto"
-        if entry["tipologia"] == "Corretto":
-            applets.append(entry)
-            n_inserted+=1
-        i+=1
-    
-    # for i in range(start, start+n_to_insert):
-    #     applets.append(json.loads(data[i]))
+
+    # Intent-variables matching
+    model = SentenceTransformer('all-MiniLM-L6-v2')
 
     to_json = {}
-
     i=1
     for applet in applets:
         print(f"Original description: {applet["original_description"]}")
@@ -82,33 +82,38 @@ if __name__ == "__main__":
         triggers = get_applet_triggers(applet)
         
         # Action
-        action = get_applet_action(applet)
+        actions = get_applet_actions(applet)
         
-        # Intent-variables matching
-        model = SentenceTransformer('all-MiniLM-L6-v2')
+        # Skip
+        skip = get_applet_skip(applet)
+
+        # Matching                
+        #res = match(model, applet["intent"], triggers)
         
-        res = match(model, applet["intent"], triggers)
         
-        # for key, value in res.items():
-        #     print(f"{key}: {value}")
+        # print("Variable names:")
+        # for elem in triggers:
+        #     print(elem)
         
-        print("Variable names:")
-        for key in res.keys():
-            print(key)
+        # print("Action methods:")
+        # for elem in actions:
+        #     print(elem)
         
-        print(f"Action method: {action}")
-        print("="*60)
+        # print(f"Skip method: {skip}")
+        
+        # print("="*60)
                 
         to_json[f"applet_{i}"] = {}
         to_json[f"applet_{i}"]["original_description"] = applet["original_description"]
         to_json[f"applet_{i}"]["intent"] = applet["intent"]
-        to_json[f"applet_{i}"]["triggers"] = list(res.keys())
-        to_json[f"applet_{i}"]["action"] = action
+        to_json[f"applet_{i}"]["triggers"] = triggers
+        to_json[f"applet_{i}"]["actions"] = actions
+        to_json[f"applet_{i}"]["skip"] = skip
         to_json[f"applet_{i}"]["filter_code"] = applet["filter_code"]
 
         i+=1
 
-    with open("data\\Json200_cleaned.json", "w") as f:
+    with open("data\\data_cleaned.json", "w") as f:
         json.dump(to_json, f, indent=3, separators=(',', ': '))
     
     print("Data saved to file.")
