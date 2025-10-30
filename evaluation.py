e236be50b89ed246b6b1f6e9432b26eea88ed403
Dar7oo <143949654+dario-falc@users.noreply.github.com>
2025-10-30 16:37:56 +0100

    Added model3 results

--- evaluation.py
+++ evaluation.py
@@ -10,13 +10,14 @@ from codebleu import calc_codebleu
 
 
 def clean_result(text):
-    pattern = r'```javascript\s*\n(.*?)(?:\n```|$)'
+    pattern = r'```(?:javascript|js)\n(.*?)```'
     
-    match = re.search(pattern, text, re.DOTALL)
+    matches = re.findall(pattern, text, re.DOTALL)
     
-    if match:
-        return match.group(1).rstrip()
-    return None
+    if matches:
+        return '\n\n'.join(matches)
+    else:
+        return text
 
 
 if __name__ == "__main__":
@@ -49,6 +50,16 @@ if __name__ == "__main__":
     model2_metrics["codebertscore"] = 0
     model2_metrics["codebleu"] = 0
     
+    
+    model3_metrics = {}
+    model3_metrics["bleu"] = 0
+    model3_metrics["rougeL"] = 0
+    model3_metrics["rouge1"] = 0
+    model3_metrics["rouge2"] = 0
+    model3_metrics["meteor"] = 0
+    model3_metrics["codebertscore"] = 0
+    model3_metrics["codebleu"] = 0
+    
 
     i=1
     for applet in applets.values():
@@ -57,14 +68,17 @@ if __name__ == "__main__":
         test_code = applet["filter_code"]
         model1_res = clean_result(applet["deepseek-ai/deepseek-coder-1.3b-instruct"])
         model2_res = clean_result(applet["Qwen/Qwen2.5-Coder-1.5B-Instruct"])
+        model3_res = clean_result(applet["google/gemma-2-2b-it"])
     
-    
+        
         # BLEU
         model1_bleu = sentence_bleu(references=[test_code], hypothesis=model1_res)
         model2_bleu = sentence_bleu(references=[test_code], hypothesis=model2_res)
+        model3_bleu = sentence_bleu(references=[test_code], hypothesis=model3_res)
         
         model1_metrics["bleu"] += model1_bleu
         model2_metrics["bleu"] += model2_bleu
+        model3_metrics["bleu"] += model3_bleu
         
         
         # ROUGE-L, ROUGE-1, ROUGE-2
@@ -77,6 +91,10 @@ if __name__ == "__main__":
         model2_rouge1 = scorer.score(target=test_code, prediction=model2_res)["rouge1"].fmeasure
         model2_rouge2 = scorer.score(target=test_code, prediction=model2_res)["rouge2"].fmeasure
         
+        model3_rougeL = scorer.score(target=test_code, prediction=model3_res)["rougeL"].fmeasure
+        model3_rouge1 = scorer.score(target=test_code, prediction=model3_res)["rouge1"].fmeasure
+        model3_rouge2 = scorer.score(target=test_code, prediction=model3_res)["rouge2"].fmeasure
+        
         model1_metrics["rougeL"] += model1_rougeL
         model1_metrics["rouge1"] += model1_rouge1
         model1_metrics["rouge2"] += model1_rouge2
@@ -84,32 +102,43 @@ if __name__ == "__main__":
         model2_metrics["rougeL"] += model2_rougeL
         model2_metrics["rouge1"] += model2_rouge1
         model2_metrics["rouge2"] += model2_rouge2
+        
+        model3_metrics["rougeL"] += model3_rougeL
+        model3_metrics["rouge1"] += model3_rouge1
+        model3_metrics["rouge2"] += model3_rouge2
 
                 
         # METEOR        
         model1_meteor = meteor_score(references=[word_tokenize(test_code)], hypothesis=word_tokenize(model1_res))
         model2_meteor = meteor_score(references=[word_tokenize(test_code)], hypothesis=word_tokenize(model2_res))
+        model3_meteor = meteor_score(references=[word_tokenize(test_code)], hypothesis=word_tokenize(model3_res))
 
         model1_metrics["meteor"] += model1_meteor
         model2_metrics["meteor"] += model2_meteor
+        model3_metrics["meteor"] += model3_meteor
         
         
         # CodeBERTScore
         model1_CBS = float(code_bert_score.score(refs=[test_code], cands=[model1_res], lang='javascript')[2][0])
         model2_CBS = float(code_bert_score.score(refs=[test_code], cands=[model2_res], lang='javascript')[2][0])
-                
+        model3_CBS = float(code_bert_score.score(refs=[test_code], cands=[model3_res], lang='javascript')[2][0])
+
         model1_metrics["codebertscore"] += model1_CBS
         model2_metrics["codebertscore"] += model2_CBS
+        model3_metrics["codebertscore"] += model3_CBS
         
         
         # CodeBLEU
         model1_CB = calc_codebleu([test_code], [model1_res], "javascript")["codebleu"]
         model2_CB = calc_codebleu([test_code], [model2_res], "javascript")["codebleu"]
+        model3_CB = calc_codebleu([test_code], [model3_res], "javascript")["codebleu"]
         
         model1_metrics["codebleu"] += model1_CB
         model2_metrics["codebleu"] += model2_CB
+        model3_metrics["codebleu"] += model3_CB
         
         i+=1
+
 
     # Calculate average
     model1_metrics["bleu"] /= n
@@ -127,11 +156,20 @@ if __name__ == "__main__":
     model2_metrics["meteor"] /= n
     model2_metrics["codebertscore"] /= n
     model2_metrics["codebleu"] /= n
+    
+    model3_metrics["bleu"] /= n
+    model3_metrics["rougeL"] /= n
+    model3_metrics["rouge1"] /= n
+    model3_metrics["rouge2"] /= n
+    model3_metrics["meteor"] /= n
+    model3_metrics["codebertscore"] /= n
+    model3_metrics["codebleu"] /= n
 
 
     metrics = {}
     metrics["model1"] = model1_metrics
     metrics["model2"] = model2_metrics
+    metrics["model3"] = model3_metrics
     
     with open("data\\metrics.json", "w") as f:
         json.dump(metrics, f, indent=3, separators=(',', ': '))
